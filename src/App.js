602941ba4cc75946b214830a5ea6d4cc69f49530
Zheka <114327298+Zhekaggg@users.noreply.github.com>
2024-01-19 16:57:35 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
+import * as React from 'react';
+import Journal from './component/Journal';
+import "./App.css"
+import "./model.css"
 
+const App = () => {
+  return (
+    <div>
+      <Journal />
+    </div>
+  );
+};
+
+export default App;
